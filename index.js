64dabf90abb8a895b10e770f31f54813b309bc45
Stefan Pla <spla@icloud.com>
2024-02-14 08:53:57 -0500

    defined pokemonCount and pokedex variables which will serve as foundation for the rest of the javascript code

---
+++ index.js
@@ -0,0 +1,4 @@
+const pokemonCount = 151;
+// can change the number above to load different number of Pokemon into the Pokedex. If time permits, maybe develop an event listener that allows the user of the application to do so. 
+
+const pokedex = {}; // using Bulbasaur as an example: {"name" : "bulbasaur", "img" : url, "type" : ["grass","poison"], "desc" : "....."} }
