4942fa81a8de2fd51a3afa62703a784338e892c8
Stefan Pla <spla@icloud.com>
2024-02-14 09:24:25 -0500

    defined a window.onload function which ensures that all divs for each pokemon are not rendered until all data is available.

--- index.js
+++ index.js
@@ -36,3 +36,30 @@ function getPokemon(num) {
           });
         });
 };
+
+window.onload = function() { 
+    // the code inside this function runs only after all the HTML, CSS, and JavaScript resources have been fully loaded. It ensures that all of the external resources, like images, are fully loaded before code executes. Since this code dynamically creates elements and sets their properties based on fetched data, we need to wait for all resources to be available to ensure everything renders correctly. Will slow down initial load time as pokemonCount gets bigger.
+    const promises = []; 
+
+    for (let i = 1;i<=pokemonCount;i++) {
+        promises.push(getPokemon(i));
+    } // resulting promise from getPokemon() is pushed into this array.
+    // console.log(pokedex);
+    Promise.all(promises).then(() => { // waits for all promises to resolve and then executes this callback function and each pokemon is rendered by creating a div for each pokemon and adding event listeners etc. This ensures that the pokemon list is not rendered until all data is available.
+        // console.log(pokedex);
+        for (let i=1;i<=pokemonCount;i++) {
+            let pokemon = document.createElement("div");
+            pokemon.id = pokedex[i].id
+            pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
+            pokemon.classList.add("pokemon-name");
+            pokemon.addEventListener("click", updatePokemon);
+            document.getElementById("pokemon-list").append(pokemon);
+
+        };
+            let imageElement = document.getElementById("pokemon-img");
+            imageElement.addEventListener("mouseover", updateImage)
+            document.getElementById("pokemon-description").innerText = pokedex[1]["desc"];
+            imageElement.src = pokedex[1]["img"]; 
+            
+        });
+};
